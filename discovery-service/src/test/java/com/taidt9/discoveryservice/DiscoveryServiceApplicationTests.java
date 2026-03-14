cf43cbdc0d7d5880548d560a5b83cb6192601f7b
ghtaidt9 <ghtaidt9@github.com>
2026-03-15 06:35:40 +0700

    Initialize eureka

---
+++ discovery-service/src/test/java/com/taidt9/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taidt9.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
