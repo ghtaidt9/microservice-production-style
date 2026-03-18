08447328eb689078c14138864e319742bef3505a
ghtaidt9 <ghtaidt9@github.com>
2026-03-18 12:20:37 +0700

    Revert OrderService

--- oder-service/src/main/java/com/taidt9/orderservice/service/OrderService.java
+++ oder-service/src/main/java/com/taidt9/orderservice/service/OrderService.java
@@ -4,9 +4,7 @@ import com.taidt9.OrderEvent;
 import com.taidt9.orderservice.OrderRepository;
 import com.taidt9.orderservice.entity.Order;
 import com.taidt9.orderservice.metrics.OrderMetrics;
-import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
-import io.micrometer.tracing.Span;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -17,46 +15,16 @@ public class OrderService {
     private final OrderRepository repo;
     private final KafkaTemplate<String, OrderEvent> kafkaTemplate;
     private final OrderMetrics orderMetrics;
-    private final Tracer tracer;
 
     public Order create(Order order) {
 
-        Span span = tracer.nextSpan().name("create-order");
         Order saved = repo.save(order);
 
-        try (Tracer.SpanInScope ws = tracer.withSpan(span.start())) {
-            processPayment(saved);
-            sendKafkaEvent(saved);
-        } finally {
-            span.end();
-        }
-
-
+        // Kafka publish
+        kafkaTemplate.send("order-topic",
+                new OrderEvent(order.getProductId(), order.getQuantity()));
+        orderMetrics.incrementOrders();
 
         return saved;
-    }
-    private void processPayment(Order order) {
-
-        Span span = tracer.nextSpan().name("process-payment");
-
-        try (Tracer.SpanInScope ws = tracer.withSpan(span.start())) {
-            // giả lập payment
-        } finally {
-            span.end();
-        }
-    }
-
-    private void sendKafkaEvent(Order order) {
-
-        Span span = tracer.nextSpan().name("send-kafka-event");
-
-        try (Tracer.SpanInScope ws = tracer.withSpan(span.start())) {
-            // Kafka publish
-            kafkaTemplate.send("order-topic",
-                    new OrderEvent(order.getProductId(), order.getQuantity()));
-            orderMetrics.incrementOrders();
-        } finally {
-            span.end();
-        }
     }
 }
