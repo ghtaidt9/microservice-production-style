6aaa8fe908efa58cd00934d5963f199cf79d121f
Đoàn Thanh Tài <118824799+ghtaidt9@users.noreply.github.com>
2026-03-20 19:47:32 +0700

    Implement distrubuted tracing (#5)
    * Implement jaeger for user-service
    
    * Distributed Tracing
    - filer logging
    - create OtlpHttpSpanExporter bean in java code
    - export to jaeger
    - scape security for prometheus
    
    ---------
    
    Co-authored-by: ghtaidt9 <ghtaidt9@github.com>

---
+++ inventory-service/src/main/java/com/taidt9/inventoryservice/config/OtlpConfig.java
@@ -0,0 +1,13 @@
+package com.taidt9.inventoryservice.config;
+
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OtlpConfig {
+    @Bean
+    OtlpHttpSpanExporter otlpHttpSpanExporter() {
+        return OtlpHttpSpanExporter.builder().build();
+    }
+}
