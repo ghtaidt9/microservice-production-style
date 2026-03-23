4eb1d235565431da72b96da83204d24d62b7a1a3
Đoàn Thanh Tài <118824799+ghtaidt9@users.noreply.github.com>
2026-03-24 06:47:19 +0700

    Listening order and payment events then push notification via sns (#8)
    Co-authored-by: ghtaidt9 <ghtaidt9@github.com>

---
+++ notification-service/src/main/java/com/taidt9/notificationservice/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.taidt9.notificationservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${monitoring.prometheus.api-key}")
+    private String prometheusApiKey;
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(CsrfConfigurer::disable).authorizeHttpRequests(auth -> auth
+
+//                .requestMatchers("/api/orders").access((authentication, context) -> {
+//                    String internal = context.getRequest().getHeader("X-INTERNAL-REQUEST");
+//                    return new AuthorizationDecision("true".equals(internal));
+//                })
+                // Prometheus endpoint
+                .requestMatchers("/actuator/prometheus").access((authen, context) -> {
+                    String authHeader = context.getRequest().getHeader("Authorization");
+                    return new AuthorizationDecision(authHeader != null && authHeader.equals(String.format("Bearer %s", prometheusApiKey)));
+                })
+                // Health & info (optional: allow internal)
+                .requestMatchers("/actuator/health", "/actuator/info").permitAll()
+                // Block other actuator endpoints
+                .requestMatchers("/actuator/**").denyAll()
+                // Business APIs vẫn dùng JWT (nếu có)
+                .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
---
+++ notification-service/src/test/java/com/taidt9/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taidt9.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
