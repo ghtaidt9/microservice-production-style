a5a57b03b7494744cac0535447f379afba504f20
ghtaidt9 <ghtaidt9@github.com>
2026-04-24 11:12:51 +0700

    temp commit for checking what changed things are after using codecursor

--- docker/db_init/init.sql
+++ docker/db_init/init.sql
@@ -5,11 +5,6 @@ CREATE DATABASE IF NOT EXISTS `order_db`;
 CREATE DATABASE IF NOT EXISTS `inventory_db`;
 CREATE DATABASE IF NOT EXISTS `analytics_db`;
 
--- Optional: Create specific users and grant privileges
--- CREATE USER 'user_one'@'%' IDENTIFIED BY 'password_one';
--- GRANT ALL PRIVILEGES ON `database_one`.* TO 'user_one'@'%';
-
--- CREATE USER 'user_two'@'%' IDENTIFIED BY 'password_two';
--- GRANT ALL PRIVILEGES ON `database_two`.* TO 'user_two'@'%';
+-- Root user is already created by Docker with MYSQL_ROOT_PASSWORD, allowing from any host
 
 FLUSH PRIVILEGES;
