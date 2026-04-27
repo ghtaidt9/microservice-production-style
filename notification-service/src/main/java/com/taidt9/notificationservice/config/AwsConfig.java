4463b0cebcfb2249593c7be1b4046df317ed384f
Đoàn Thanh Tài <118824799+ghtaidt9@users.noreply.github.com>
2026-04-27 15:21:13 +0700

    K8s impl (#10)
    * temp commit for checking what changed things are after using codecursor
    
    * 17.7 impl ingress
    
    * Implement config map for all other services
    
    * Covered the lost env in configmap
    
    ---------
    
    Co-authored-by: ghtaidt9 <ghtaidt9@github.com>

--- notification-service/src/main/java/com/taidt9/notificationservice/config/AwsConfig.java
+++ notification-service/src/main/java/com/taidt9/notificationservice/config/AwsConfig.java
@@ -2,6 +2,7 @@ package com.taidt9.notificationservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -12,10 +13,13 @@ import java.net.URI;
 @Configuration
 public class AwsConfig {
 
+    @Value("${AWS_SNS_ENDPOINT:http://localstack:4566}")
+    private String snsEndpoint;
+
     @Bean
     public SnsClient snsClient() {
         return SnsClient.builder()
-                .endpointOverride(URI.create("http://localhost:4566")) // LocalStack endpoint
+                .endpointOverride(URI.create(snsEndpoint))
                 .region(Region.US_EAST_1)
                 .credentialsProvider(
                         StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test"))
